c9c9a7d41a99557de68acb981b025eee58b0be69
Daniel Greene <daniel.b.greene@gmail.com>
2015-11-06 19:29:25 -0500

    upgrading express

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
+var http = require('http');
+var path = require('path');
 var express = require('express');
+
 var app = express();
 
+app.set('port', process.env.PORT || 3000);
+
 //Create a static file server
-app.configure(function() {
-  app.use(express.static(__dirname + '/public'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Get the dummy data
 require('./server/ddata.js');
 
-var port = 8080;
-app.listen(port);
-console.log('Express server started on port %s', port);
+var server = http.createServer(app);
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
