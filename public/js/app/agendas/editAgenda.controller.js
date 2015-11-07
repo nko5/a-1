f722297b3a95eefee409989301bcab93286c0d59
Daniel Greene <daniel.b.greene@gmail.com>
2015-11-07 16:32:08 -0500

    geolocation cleanup in edit agenda

--- public/js/app/agendas/editAgenda.controller.js
+++ public/js/app/agendas/editAgenda.controller.js
@@ -1,24 +1,31 @@
 import _ from "npm:lodash@3.10.1";
 
 export default function($location, $http, agenda, geolocation, locationService) {
-    var vm = this;
+  var vm = this;
 
-    vm.agenda = agenda;
+  vm.agenda = agenda;
 
-    vm.createTask = createTask;
+  vm.createTask = createTask;
 
-    geolocation.getLocation().then(function(data){
-      vm.currentCoordinates = {lat:data.coords.latitude, long:data.coords.longitude};
+  populateAddressFromGeolocation();
+
+  function populateAddressFromGeolocation() {
+    geolocation.getLocation().then(function(data) {
+      vm.currentCoordinates = {
+        lat: data.coords.latitude,
+        long: data.coords.longitude
+      };
       locationService.getAddressByLatLon(vm.currentCoordinates.lat,
-      vm.currentCoordinates.long).then(function(data) {
+        vm.currentCoordinates.long).then(function(data) {
         vm.startAddress = _.find(data.data.results, function(locationResult) {
           return _.includes(locationResult.types, "street_address");
         }).formatted_address;
       });
     });
+  }
 
-    function createTask($event) {
-        $event.preventDefault();
-        $location.path('/agendas/' + agenda.id + '/task');
-    }
+  function createTask($event) {
+    $event.preventDefault();
+    $location.path('/agendas/' + agenda.id + '/task');
+  }
 }
