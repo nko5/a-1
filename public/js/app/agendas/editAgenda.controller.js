d0436fc37dbb8341c9fe691e8b176f0432b0fce0
Daniel Greene <daniel.b.greene@gmail.com>
2015-11-07 16:50:12 -0500

    auto populate geo on new agenda

--- public/js/app/agendas/editAgenda.controller.js
+++ public/js/app/agendas/editAgenda.controller.js
@@ -1,19 +1,25 @@
-export default function($location, $routeParams, Agenda) {
+import _ from 'lodash';
+
+export default function($location, $routeParams, Agenda, geolocation,
+  locationService) {
   var vm = this;
 
   init();
 
   function init() {
-    if($routeParams.agendaId) {
-      Agenda.get({id: $routeParams.agendaId}).$promise.then(function (agenda) {
+    if ($routeParams.agendaId) {
+      Agenda.get({
+        id: $routeParams.agendaId
+      }).$promise.then(function(agenda) {
         vm.agenda = agenda;
       });
     } else {
       vm.agenda = new Agenda();
+      populateStartAddressFromGeolocation(vm.agenda);
     }
   }
 
-  vm.createAgenda = function () {
+  vm.createAgenda = function() {
     console.log('inside save');
     var Model = $routeParams.agendaId ? vm.agenda.$update() : vm.agenda.$save();
     Model.then(function(success) {
@@ -26,6 +32,17 @@ export default function($location, $routeParams, Agenda) {
   function createTask($event) {
     $event.preventDefault();
     $location.path('/agendas/' + agenda.id + '/task');
+  }
+
+  function populateStartAddressFromGeolocation(agenda) {
+    geolocation.getLocation().then(function(data) {
+      locationService.getAddressByLatLon(data.coords.latitude,
+        data.coords.longitude).then(function(data) {
+        agenda.startAddress = _.find(data.data.results, function(locationResult) {
+          return _.includes(locationResult.types, "street_address");
+        }).formatted_address;
+      });
+    });
   }
 
 }
