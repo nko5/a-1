b35f7e2c9ed88029458ecf68dcb30c5bae234446
Prathamesh Satpute <prathamesh.satpute@gmail.com>
2015-11-06 23:53:07 -0500

    Added movies api

---
+++ controllers/moviesController.js
@@ -0,0 +1,10 @@
+var showtimes = require('showtimes');
+
+module.exports.getTheaters = function(req, res) {
+
+  var st = showtimes(req.body.location || '06042', {});
+  st.getTheaters(function(err, theaters) {
+    res.json(theaters);
+  });
+
+};
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const profileController = require('../controllers/profileController');
+const moviesController = require('../controllers/moviesController');
+
+router.route('/movies').get(moviesController.getTheaters);
 
 router.use(authController.ensureAuthenticated);
 router.route('/profile').get(profileController.getUserAccount);
+
 
 module.exports = router;
