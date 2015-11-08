d958ad07e59ebb0dd331a4a326417b4d16474f79
tomjnunes <tom@nunes6.net>
2015-11-08 18:07:22 -0500

    Added timeline

--- public/js/app/agendas/agenda.controller.js
+++ public/js/app/agendas/agenda.controller.js
@@ -1,9 +1,26 @@
+import _ from 'lodash';
+
 export default function($location, $route, $routeParams, AgendaService, Task) {
   var vm = this;
 
   init();
 
   function init() {
+    vm.hours = _.times(50, function(ix){
+      let hour = ix % 24;
+      let ampm = hour < 12 ? 'am' : 'pm';
+      hour = ix % 12;
+      if (hour) {
+        hour = ix + ":00";
+      }
+      else {
+        hour = "12:00"
+      }
+
+      return {value: hour + ampm};
+
+    });
+
     if ($routeParams.agendaId) {
       AgendaService.get($routeParams.agendaId).then(function(agenda) {
         vm.agenda = agenda;
