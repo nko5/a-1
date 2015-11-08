9645006756e4535c1782426130d537135f0850fc
Daniel Greene <daniel.b.greene@gmail.com>
2015-11-08 15:36:27 -0500

    encode uri for lat long

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -5,7 +5,7 @@ var _ = require('lodash');
 module.exports.getLocationDetails = function(req, res) {
   let lat = req.query.lat;
   let long = req.query.long;
-  var url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${process.env.GOOGLE_API_KEY}`;
+  var url = encodeURI(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=${process.env.GOOGLE_API_KEY}`);
   request.get(url, {
     json: true
   }, function(err, response, responseBody) {
